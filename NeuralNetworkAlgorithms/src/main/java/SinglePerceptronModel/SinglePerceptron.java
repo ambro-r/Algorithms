bdfb753bc52a59ab52828050d437c25b0ceacd5c
ambro-r <rrambrosio@gmail.com>
2020-03-26 13:27:00 +0200

    Single Perceptron Model, the neural network

---
+++ NeuralNetworkAlgorithms/src/main/java/SinglePerceptronModel/SinglePerceptron.java
@@ -0,0 +1,43 @@
+package SinglePerceptronModel;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+public class SinglePerceptron {
+
+    @Getter
+    private float[] weights;
+
+    public SinglePerceptron() {
+    }
+
+    public void train(float[][] input, float[] output, float learningRate) {
+        int numOfWeights = input[0].length;
+        this.weights = new float[numOfWeights];
+        for(int i = 0; i < weights.length; i++) weights[i] = 0;
+
+        float totalError = 1; // 100% error at the beginning of training.
+        while (totalError != 0) {
+            totalError = 0;
+            for(int i = 0; i < output.length; i++) {
+                float calculatedOutput = calculateOutput(input[i]);
+                float error = Math.abs(output[i] - calculatedOutput);
+                totalError += error;
+                for(int j = 0; j < numOfWeights; j++) {
+                    weights[j] = weights[j] + learningRate * input[i][j] * error;
+                }
+            }
+            System.out.println(String.format("Total error is %f, weights %s", totalError, Arrays.toString(weights)));
+        }
+    }
+
+    public float calculateOutput(float[] input) {
+        float sum = 0f;
+        for(int i = 0; i < input.length; i++) {
+            sum = sum + weights[i] * input[i];
+        }
+        return ActivationFunction.stepFunction(sum);
+    }
+
+}
---
+++ NeuralNetworkAlgorithms/src/test/java/SinglePerceptronModel/Test_SinglePerceptron.java
@@ -0,0 +1,29 @@
+package SinglePerceptronModel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test_SinglePerceptron {
+
+    private float[][] input = { {0,0}, {0,1}, {1,0}, {1,1} };
+    private float[] output = { 0,0,0,1 }; // Logical AND Function output for the given input
+
+    @Test
+    public void test_train() {
+        SinglePerceptron singlePerceptron = new SinglePerceptron();
+        singlePerceptron.train(input, output, 0.1f);
+        assertArrayEquals(new float[] { 0.5f, 0.5f}, singlePerceptron.getWeights());
+    }
+
+    @Test
+    public void test_calculateOutput() {
+        SinglePerceptron singlePerceptron = new SinglePerceptron();
+        singlePerceptron.train(input, output, 0.1f);
+        assertEquals(0, singlePerceptron.calculateOutput(new float[]{0,0}));
+        assertEquals(0, singlePerceptron.calculateOutput(new float[]{0,1}));
+        assertEquals(0, singlePerceptron.calculateOutput(new float[]{1,0}));
+        assertEquals(1, singlePerceptron.calculateOutput(new float[]{1,1}));
+    }
+
+}
